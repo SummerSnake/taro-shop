92d093a94a70d4661ac7178b5da1a880ca39aa73
summersnake <33661226lll@163.com>
2019-01-29 14:59:16 +0800

    登陆

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,9 +1,17 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View } from '@tarojs/components';
-import { AtButton } from 'taro-ui';
+import { AtButton, AtInput } from 'taro-ui';
+import apiUrl from '../../utils/api';
 import './index.less';
 
 export default class Index extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      userVal: '',
+      passwordVal: '',
+    };
+  }
 
   config = {
     navigationBarTitleText: '首页',
@@ -16,6 +24,46 @@ export default class Index extends Component {
       success: (res) => {
         console.log(res.code);
       }
+    });
+  }
+
+  /**
+   * 用户名输入框
+   */
+  handleUserChange(value) {
+    this.setState({
+      userVal: value,
+    });
+  }
+
+  /**
+   * 密码输入框
+   */
+  handlePasswordChange(value) {
+    this.setState({
+      passwordVal: value,
+    });
+  }
+
+  /**
+   * 提交登陆
+   */
+  onSubmit() {
+    Taro.request({
+      url: `${apiUrl}/mock/5c47cf65f513860f4ceef6a3/example/taroMini/login`,
+      data: {
+        username: this.state.userVal.toString(),
+        password: this.state.passwordVal.toString(),
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json'
+      }
+    }).then((res) => {
+      console.log(res);
+      // this.setState({
+      //   fetchData: res.data.data
+      // });
     });
   }
 
@@ -34,7 +82,7 @@ export default class Index extends Component {
   goPersonCenter = () => {
     // 获取用户信息
     Taro.getUserInfo({
-      success: (res)=>{
+      success: (res) => {
         Taro.setStorageSync('userInfo', res.userInfo);
       }
     });
@@ -48,10 +96,30 @@ export default class Index extends Component {
     return (
       <View className='homeWrap'>
         <View className='btnWrap'>
-          <AtButton type='secondary' onClick={this.goGoodList}>商品列表</AtButton>
+        <AtButton type='secondary' onClick={this.goGoodList}>商品列表</AtButton>
         </View>
         <View className='btnWrap'>
-          <AtButton type='secondary' onClick={this.goPersonCenter}>个人中心</AtButton>
+        <AtButton type='secondary' onClick={this.goPersonCenter}>个人中心</AtButton>
+        </View>
+
+        <View className='loginWrap' style={{ display: 'none' }}>
+          <AtInput
+            name='username'
+            title='用户名'
+            type='number'
+            placeholder='请输入用户名'
+            value={this.state.userVal}
+            onChange={this.handleUserChange.bind(this)}
+          />
+          <AtInput
+            name='password'
+            title='密码'
+            type='text'
+            placeholder='请输入密码'
+            value={this.state.passwordVal}
+            onChange={this.handlePasswordChange.bind(this)}
+          />
+          <AtButton type='primary' className='submitBtn' onClick={this.onSubmit}>提交</AtButton>
         </View>
       </View>
     );
