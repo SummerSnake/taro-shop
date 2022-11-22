85cc34a668a14332efa84916a05344fc6b1d0a04
李岩 <33661226lll@163.com>
2022-11-22 16:22:40 +0800

    fix: 修复循环执行sql异步的问题

--- back/nodejs/services/orderService.js
+++ back/nodejs/services/orderService.js
@@ -5,80 +5,105 @@ const { querySql } = require('../utils/mysql');
 const { CODE_ERROR, CODE_SUCCESS } = require('../config/codeConfig');
 
 /**
+ * @desc 查询订单中的商品
+ * @param { string } goodIds
+ * @return { Promise }
+ */
+const getGoodsList = async (goodIds) => {
+  const goodListSql = `SELECT * FROM good WHERE FIND_IN_SET(id, '${goodIds}')`;
+  const goodRes = await querySql(goodListSql);
+
+  if (Array.isArray(goodRes)) {
+    const goodList = JSON.parse(JSON.stringify(goodRes));
+    const goodListVo = [];
+
+    goodList.forEach((item) => {
+      const goodVo = {
+        id: item?.id,
+        title: item?.title,
+        price: item?.price,
+        imgUrl: item?.img_url,
+        description: item?.description,
+        type: item?.type,
+        isActivity: item?.is_activity,
+        salesVolume: item?.sales_volume,
+        imgList: item?.img_list,
+        createTime: item?.create_time,
+      };
+
+      goodListVo.push(goodVo);
+    });
+
+    return new Promise((resolve) => resolve(goodListVo));
+  }
+};
+
+/**
+ * @desc 查询订单列表
+ * @param { number } orderStatus
+ * @return { Promise }
+ */
+const getOrderList = async (orderStatus) => {
+  const sql = 'SELECT * FROM `order` WHERE order_status=';
+  const orderSql = orderStatus > 0 ? `${sql}${orderStatus}` : 'SELECT * FROM `order`';
+  const orderRes = await querySql(orderSql);
+
+  if (Array.isArray(orderRes)) {
+    const orderList = JSON.parse(JSON.stringify(orderRes));
+    const orderListVo = [];
+
+    orderList.forEach((item) => {
+      const orderVo = {
+        id: item?.id,
+        orderNumber: item?.order_number,
+        orderStatus: item?.order_status,
+        orderAmount: item?.order_amount,
+        goodIds: item?.good_ids,
+        createTime: item?.create_time,
+      };
+
+      orderListVo.push(orderVo);
+    });
+
+    return new Promise((resolve) => resolve(orderListVo));
+  }
+};
+
+/**
  * @desc 订单列表查询接口
  */
-function getOrderListApi(req, res, next) {
-  if (req) {
-    const {
-      query: { orderStatus = 0 },
-    } = req;
+async function getOrderListApi(req, res, next) {
+  if (!req) {
+    res.json({
+      code: CODE_ERROR,
+      msg: '请求失败',
+      data: null,
+    });
+  }
 
-    const sql = 'SELECT * FROM `order` WHERE order_status=';
-    const orderSql = orderStatus > 0 ? `${sql}${orderStatus}` : 'SELECT * FROM `order`';
+  const {
+    query: { orderStatus = 0 },
+  } = req;
 
-    querySql(orderSql).then((orderRes) => {
-      if (orderRes) {
-        const orderList = JSON.parse(JSON.stringify(orderRes));
-        const orderListVo = [];
-        orderList.forEach((item) => {
-          // 查询订单中的商品
-          const goodsListSql = `SELECT * FROM good WHERE FIND_IN_SET(id, '${item?.good_ids}')`;
+  const orderList = await getOrderList(orderStatus);
+  const goodList = await Promise.all(orderList.map((item) => getGoodsList(item?.goodIds)));
 
-          querySql(goodsListSql).then((_goodRes) => {
-            if (_goodRes) {
-              const goodsList = JSON.parse(JSON.stringify(_goodRes));
+  for (let i = 0; i < orderList.length; i++) {
+    orderList[i].goodsList = goodList[i];
+  }
 
-              const goodsListVo = [];
-              goodsList.forEach((child) => {
-                const goodVo = {
-                  id: child?.id,
-                  title: child?.title,
-                  price: child?.price,
-                  imgUrl: child?.img_url,
-                  description: child?.description,
-                  type: child?.type,
-                  isActivity: child?.is_activity,
-                  salesVolume: child?.sales_volume,
-                  imgList: child?.img_list,
-                  createTime: child?.create_time,
-                };
-
-                goodsListVo.push(goodVo);
-              });
-
-              const itemVo = {
-                id: item?.id,
-                orderNumber: item?.order_number,
-                orderStatus: item?.order_status,
-                orderAmount: item?.order_amount,
-                goodIds: item?.good_ids,
-                goodsList: goodsListVo,
-                createTime: item?.create_time,
-              };
-
-              orderListVo.push(itemVo);
-            }
-          });
-        });
-
-        data = {
-          list: orderListVo,
-        };
-
-        if (Object.keys(data).length > 0) {
-          res.json({
-            code: CODE_SUCCESS,
-            msg: '请求成功',
-            data,
-          });
-        } else {
-          res.json({
-            code: CODE_ERROR,
-            msg: '服务器错误',
-            data: null,
-          });
-        }
-      }
+  const data = { list: orderList };
+  if (Object.keys(data).length > 0) {
+    res.json({
+      code: CODE_SUCCESS,
+      msg: '请求成功',
+      data,
+    });
+  } else {
+    res.json({
+      code: CODE_ERROR,
+      msg: '服务器错误',
+      data: null,
     });
   }
 }
